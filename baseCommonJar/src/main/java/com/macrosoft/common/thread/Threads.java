5a8dbf5adb312620c0bd119f173812f7b99f73ce
呆呆 <@WangMachine>
2014-12-22 22:39:38 +0800

    commit

--- baseCommonJar/src/main/java/com/macrosoft/common/thread/Threads.java
+++ baseCommonJar/src/main/java/com/macrosoft/common/thread/Threads.java
@@ -1,51 +1,53 @@
-/**
- * Copyright (c) 2005-2012 springside.org.cn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- */
 package com.macrosoft.common.thread;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 线程相关工具类.
+ * 线程相关工具类
  * 
- * @author xiao
+ * @author 呆呆
  */
 public class Threads {
 
 	/**
 	 * sleep等待,单位为毫秒,忽略InterruptedException.
+	 * 
+	 * @param millis
 	 */
 	public static void sleep(long millis) {
 		try {
 			Thread.sleep(millis);
 		} catch (InterruptedException e) {
+			e.printStackTrace();
 			// Ignore.
 		}
 	}
 
 	/**
 	 * sleep等待,忽略InterruptedException.
+	 * 
+	 * @param duration
+	 * @param unit
 	 */
 	public static void sleep(long duration, TimeUnit unit) {
 		try {
 			Thread.sleep(unit.toMillis(duration));
 		} catch (InterruptedException e) {
+			e.printStackTrace();
 			// Ignore.
 		}
 	}
 
 	/**
+	 * 未完成(wangjun) 
 	 * 按照ExecutorService JavaDoc示例代码编写的Graceful Shutdown方法.
-	 * 先使用shutdown, 停止接收新任务并尝试完成所有已存在任务.
-	 * 如果超时, 则调用shutdownNow, 取消在workQueue中Pending的任务,并中断所有阻塞函数.
-	 * 如果仍人超時，則強制退出.
+	 * 先使用shutdown, 停止接收新任务并尝试完成所有已存在任务. 如果超时, 则调用shutdownNow,
+	 * 取消在workQueue中Pending的任务,并中断所有阻塞函数. 如果仍人超時，則強制退出.
 	 * 另对在shutdown时线程本身被调用中断做了处理.
 	 */
-	public static void gracefulShutdown(ExecutorService pool, int shutdownTimeout, int shutdownNowTimeout,
-			TimeUnit timeUnit) {
+	public static void gracefulShutdown(ExecutorService pool,
+			int shutdownTimeout, int shutdownNowTimeout, TimeUnit timeUnit) {
 		pool.shutdown(); // Disable new tasks from being submitted
 		try {
 			// Wait a while for existing tasks to terminate
@@ -65,9 +67,12 @@ public class Threads {
 	}
 
 	/**
-	 * 直接调用shutdownNow的方法, 有timeout控制.取消在workQueue中Pending的任务,并中断所有阻塞函数.
+	 * 未完成(wangjun) 
+	 * 直接调用shutdownNow的方法,
+	 * 有timeout控制.取消在workQueue中Pending的任务,并中断所有阻塞函数.
 	 */
-	public static void normalShutdown(ExecutorService pool, int timeout, TimeUnit timeUnit) {
+	public static void normalShutdown(ExecutorService pool, int timeout,
+			TimeUnit timeUnit) {
 		try {
 			pool.shutdownNow();
 			if (!pool.awaitTermination(timeout, timeUnit)) {
